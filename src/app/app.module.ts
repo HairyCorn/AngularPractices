6d458a6ffcdb84bac2b54a907d551dd693c679cd
HairyCorn <ariel.carrillo96@gmail.com>
2023-05-14 20:55:37 +0200

    Clean experiments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextInputComponent } from './text-input/text-input.component';
-import { SelectColorComponent } from './select-color/select-color.component';
-import { ColorChromeModule } from 'ngx-color/chrome';
-import { NgxColorsModule } from 'ngx-colors';
-import { SelectColorsComponent } from './select-colors/select-colors.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -17,17 +11,12 @@ import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TextInputComponent,
-    SelectColorComponent,
-    SelectColorsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ColorChromeModule,
-    NgxColorsModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
