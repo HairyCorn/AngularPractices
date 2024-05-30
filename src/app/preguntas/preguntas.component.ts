4ae39e1b91479a0fa58c1e4aadd1bdfb8b352810
HairyCorn <ariel.carrillo96@gmail.com>
2024-05-30 22:11:16 +0200

    Update package.json

--- src/app/preguntas/preguntas.component.ts
+++ src/app/preguntas/preguntas.component.ts
@@ -10,22 +10,22 @@ export class PreguntasComponent implements OnInit {
   constructor(private http: HttpClient) {}
   question: string = 'Hola mundo';
   topic: string | null = null;
-  jsonData: Preguntas | undefined;
+  jsonData: Preguntas[] | undefined;
   respuestas: string[] = [];
   disableCheck: boolean = false;
-
+  preguntaActual: Preguntas | undefined;
   ngOnInit(): void {
     this.http.get<Preguntas[]>('assets/preguntas.json').subscribe(
       (datos) => {
-        const numeroPregunta = this.getRandomInt(datos.length);
-        this.jsonData = datos[numeroPregunta];
+        this.jsonData = datos;
+        this.renderPregunta();
       },
       (error) => {
         console.log('Error al cargar el archivo JSON', error);
       }
     );
   }
-  private getRandomInt(lenght: number) {
+  private getRandomInt(lenght: any) {
     return Math.floor(Math.floor(Math.random() * lenght));
   }
 
@@ -35,5 +35,14 @@ export class PreguntasComponent implements OnInit {
 
   onCheckboxChange() {
     this.disableCheck = true;
+  }
+  renderPregunta() {
+    const numeroPregunta = this.getRandomInt(this.jsonData?.length);
+    if (this.jsonData != undefined) {
+      this.preguntaActual = this.jsonData[numeroPregunta];
+      this.jsonData.splice(numeroPregunta, 1);
+      this.disableCheck = false;
+    }
+    console.log(this.jsonData, this.preguntaActual);
   }
 }
